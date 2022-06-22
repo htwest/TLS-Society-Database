4e54feec5922d681434b09ccba2a09fba380d97a
Hayden West <haydenw7@gmail.com>
2022-06-22 13:22:36 -0700

    created new branch

--- client/src/components/Landing.page.js
+++ client/src/components/Landing.page.js
@@ -1,21 +1,14 @@
 import React, { Fragment } from "react";
-<<<<<<< HEAD
-// import Login from "../components/landing/Login";
-=======
 import Login from "../components/landing/Login";
->>>>>>> login
 import Register from "../components/landing/Register";
 
 export const LandingPage = () => {
   return (
     <Fragment>
       <h1>Landing Page</h1>
-<<<<<<< HEAD
-=======
       <br />
       <Login />
       <br />
->>>>>>> login
       <Register />
     </Fragment>
   );
