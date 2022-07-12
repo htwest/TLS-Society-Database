7c684601f6135b774c2591d48b1a31016b23e17e
Hayden West <haydenw7@gmail.com>
2022-07-11 19:08:05 -0700

    refactor

--- index.js
+++ index.js
@@ -36,22 +36,6 @@ app.use(
 );
 app.use(express.json());
 
-<<<<<<< HEAD
-app.use(
-  session({
-    secret: "secretcode",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
-
-app.use(cookieParser("secretcode"));
-app.use(passport.initialize());
-app.use(passport.session());
-require("./passportConfig")(passport);
-
-=======
->>>>>>> sessionV2
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 }
@@ -63,99 +47,8 @@ if (process.env.NODE_ENV === "production") {
 // **** AUTHENTICATION ****
 app.use("/auth", authRouter);
 
-<<<<<<< HEAD
-// Create a New User
-app.post("/signup", async (req, res) => {
-  try {
-    //  Password Hash
-    const hashedPassword = await bcrypt.hash(req.body.user_password, 10);
-
-    // Sign Up Info
-    const name = req.body.user_name;
-    const fname = req.body.user_fname;
-    const lname = req.body.user_lname;
-    const email = req.body.user_email;
-    const password = hashedPassword;
-    const approved = false;
-    const mod = false;
-
-    // Check if Username is Taken
-    const userCheck = await pool.query(
-      `SELECT * FROM users WHERE user_name = $1`,
-      [name]
-    );
-
-    if (userCheck.rows.length > 0) {
-      res.send("User Name Already Taken");
-    } else {
-      // Add User to DB
-      const newUser = await pool.query(
-        "INSERT INTO users (user_name, user_fname, user_lname, user_email, user_password, user_approved,  user_mod) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *",
-        [name, fname, lname, email, password, approved, mod]
-      );
-      res.send("User Created");
-    }
-  } catch (err) {
-    res.sendStatus(500);
-    console.log(err);
-  }
-});
-
-// Log In
-// app.post("/userlogin", async (req, res) => {
-//   const name = req.body.user_name;
-//   const password = req.body.user_password;
-
-//   const user = await pool.query("SELECT * FROM users WHERE user_name = $1", [
-//     name,
-//   ]);
-
-//   if (user.rows.length > 0) {
-//     try {
-//       if (await bcrypt.compare(password, user.rows[0].user_password)) {
-//         res.send("Logged In");
-//       } else {
-//         res.send("Wrong Username/Password");
-//       }
-//     } catch (err) {
-//       res.sendStatus(500);
-//       console.log(err);
-//     }
-//   } else {
-//     res.send("Not a user");
-//   }
-// });
-
-app.post("/userlogin", async (req, res, next) => {
-  const name = req.body.user_name;
-  const password = req.body.user_password;
-
-  passport.authenticate("local", (err, user, info) => {
-    // console.log(user);
-    // Coming back as "false";
-    if (err) {
-      console.log(err);
-      res.send(err);
-    }
-    if (!user) {
-      res.send(user);
-    } else {
-      req.login(user, (err) => {
-        if (err) {
-          console.log(err);
-          res.send(err);
-        } else {
-          res.send("Successfully Authenticated");
-          console.log(req.user);
-        }
-      });
-    }
-  })(req, res, next);
-});
-=======
 // **** TODOS ****
 app.use("/todo", todoRouter);
->>>>>>> sessionV2
 
 // **** MODERATION ****
 app.use("/user", modRouter);
