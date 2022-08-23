b76240881ce7e48cce6806c3a29ce5109003a472
Hayden West <haydenw7@gmail.com>
2022-08-23 14:52:31 -0700

    added header

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import WhiteLogo from "../../images/WhiteLogo.png";
+
+const Header = () => {
+  return (
+    <div className="header-box">
+      <img src={WhiteLogo} alt="logo" />
+    </div>
+  );
+};
+
+export default Header;
